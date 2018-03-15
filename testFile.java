5a49c1e964cfc55d7bf1957d95860631292d60a9
Ridge Reventar <ridge.reventar@gmail.com>
2018-03-15 16:06:11 -0400

    added changes to beta branch

--- testFile.java
+++ testFile.java
@@ -1,5 +1,6 @@
 public class testFile {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("changes made for the beta branch testing");
     }
 }
